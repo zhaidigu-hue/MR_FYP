4c9ae8a48ef711be8d6621b964e283e41e80a569
ChanTszChun147 <234408289+zhaidigu-hue@users.noreply.github.com>
2026-02-27 18:11:59 +0800

    TowerSetting

---
+++ Assets/EnermyDetector.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnermyDetector : MonoBehaviour
+{
+    public Collider EnermyDetectorCollider;
+    
+}
